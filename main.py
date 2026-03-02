8352e635138a9ddf2e900dbf4feb06c04a9095e7
DangerMaster451 <93455384+DangerMaster451@users.noreply.github.com>
2026-03-01 17:07:27 -0800

    setup some classes

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+from enum import Enum
+from time import struct_time
 
+class Status(Enum):
+    NOT_STARTED = "not_started"
+    STARTED = "started"
+    COMPLETE = "complete"
 
+class Task:
+    def __init__(self, title:str, description:str, status:Status) -> None:
+        self.title:str = title
+        self.description:str = description
+        self.status:Status = status
 
+    @staticmethod
+    def convertToStructured(task:UnstructuredTask, startTime:struct_time, endTime:struct_time) -> StructuredTask:
+        return StructuredTask(task.title, task.description, task.status, startTime, endTime)
+    
+    @staticmethod
+    def convertToUnStructured(task:StructuredTask) -> UnstructuredTask:
+        return UnstructuredTask(task.title, task.description, task.status)
 
-class 
+class UnstructuredTask(Task):
+    def __init__(self, title:str, description:str, status:Status):
+        super().__init__(title, description, status)
+
+class StructuredTask(Task):
+    def __init__(self, title:str, description:str, status:Status, startTime:struct_time, endTime:struct_time):
+        super().__init__(title, description, status)
+        self.startTime = startTime
+        self.endTime = endTime
