652bf9cf08fa858b6a7108921f734e89682bed19
DangerMaster451 <93455384+DangerMaster451@users.noreply.github.com>
2026-03-01 19:06:02 -0800

    added some double booking validation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from datetime import date, datetime, timedelta
+from datetime import date, time, datetime
 
 class Status(Enum):
     NOT_STARTED = "not started"
@@ -13,7 +13,7 @@ class Task:
         self.status:Status = status
 
     @staticmethod
-    def convertToStructured(task:UnstructuredTask, startTime:datetime, endTime:datetime) -> StructuredTask:
+    def convertToStructured(task:UnstructuredTask, startTime:time, endTime:time) -> StructuredTask:
         return StructuredTask(task.title, task.description, task.status, startTime, endTime)
     
     @staticmethod
@@ -25,14 +25,32 @@ class UnstructuredTask(Task):
         super().__init__(title, description, status)
 
 class StructuredTask(Task):
-    def __init__(self, title:str, description:str, status:Status, startTime:datetime, endTime:datetime):
+    def __init__(self, title:str, description:str, status:Status, startTime:time, endTime:time):
         super().__init__(title, description, status)
         self.startTime = startTime
         self.endTime = endTime
 
+class TaskList(list[StructuredTask]):
+    def __init__(self):
+        super().__init__(self)
+    
+    def _validate(self, item:StructuredTask) -> bool:
+        for i in self:
+            if i.startTime < item.startTime < i.endTime:
+                return False
+            if i.startTime < item.endTime < i.endTime:
+                return False
+        return True
+
+    def append(self, item):
+        if self._validate(item):
+            super().append(item)
+        else:
+            raise ValueError("Cannot double book times")
 class Day():
-    def __init__(self, date:date, tasks:list[StructuredTask] = []):
-        self.tasks:list[StructuredTask] = tasks
+    def __init__(self, date:date):
+        self.date = date
+        self.tasks:TaskList = TaskList()
 
     def sortTasks(self):
         self.tasks.sort(key = lambda task: task.startTime)
@@ -43,13 +61,14 @@ class Day():
             print(task.title)
             print(task.description)
             print(task.status)
-            print(f"{task.startTime.time()} - {task.endTime.time()}")
+            print(f"{task.startTime.strftime("%H:%M:%S")} - {task.endTime.strftime("%H:%M:%S")}")
             print()
 
 
-d = Day(date.today(), [StructuredTask("Task 1", "Stuff", Status.NOT_STARTED, datetime.now(), datetime(2026, 3, 1, 19, 0, 0)),
-                   StructuredTask("Task 2", "More Stuff", Status.IN_PROGRESS, datetime(2026, 3, 1, 19, 0, 0), datetime(2026, 3, 1, 20, 0, 0)),
-                   StructuredTask("Task 3", "Even More Stuff", Status.IN_PROGRESS, datetime(2026, 3, 1, 11, 0, 0), datetime(2026, 3, 1, 12, 0, 0))
-                   ])
+d = Day(date.today())
+
+d.tasks.append(StructuredTask("Task 1", "Stuff", Status.NOT_STARTED, time.fromisoformat("07:15:00"), time.fromisoformat("08:00:00")))
+d.tasks.append(StructuredTask("Task 2", "More Stuff", Status.IN_PROGRESS, time.fromisoformat("08:00:00"), time.fromisoformat("08:30:00")))
+d.tasks.append(StructuredTask("Task 3", "Even More Stuff", Status.IN_PROGRESS, time.fromisoformat("08:30:00"), time.fromisoformat("08:45:00")))
 
 d.displayTasks()
