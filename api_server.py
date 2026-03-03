86d4c65b97e446d8168436cdfa940aec983684ab
DangerMaster451 <93455384+DangerMaster451@users.noreply.github.com>
2026-03-02 21:46:52 -0800

    added sessions

--- api_server.py
+++ api_server.py
@@ -1,10 +1,13 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel
 from tasks import Task, UnscheduledTask, ScheduledTask, TaskList, Status
-from datetime import date, time
+from datetime import date, time, datetime, timedelta
+from session import Session, SessionList
 import json
+import uuid
 
 task_list = TaskList()
+session_list = SessionList()
 
 task_list.append(ScheduledTask(title="Task 1", description="This is task 1", status=Status.NOT_STARTED, date=date(2026, 3, 2), startTime=time(9, 0), endTime=time(10, 0)))
 
@@ -15,20 +18,24 @@ async def root():
     return { "message": "Home" }
 
 @app.post("/log-in")
-async def log_in(username:str, password:str):
+async def log_in(request:Request, username:str, password:str):
     with open("users.json", "r") as file:
         users = json.load(file)
         if username in users:
             if users[username]["password"] == password:
-                return { "message":"successfully logged in!"}
+                return { "session":Session(username, request.client.host, session_list)} #type:ignore
             else:
                 raise HTTPException(status_code=403, detail=f"Incorrect Password, try '{users[username]["password"]}'")
         else:
             raise HTTPException(status_code=404, detail="User not found :/")
 
 @app.get("/all-tasks")
-async def tasks():
-    return { "message":task_list }
+async def tasks(request:Request, token:uuid.UUID):
+    v = session_list.validateSession(request, token)
+    if v:
+        return { "message":task_list }
+    else:
+        return {"validated ": v}
 
 @app.post("/add-task")
 async def add_task(task:ScheduledTask):
--- client.py
+++ client.py
@@ -1,11 +1,12 @@
+from tasks import Task, TaskList
+from session import Session
 from getpass import getpass
 import requests
-
+import uuid
 
 url = "http://127.0.0.1:8000"
 
-
-def login():
+def login() -> str:
     print("Please Log In:")
     username = input("Username: ")
     password = getpass(prompt="Password: ")
@@ -13,8 +14,33 @@ def login():
     r = requests.post(f"{url}/log-in?username={username}&password={password}")
 
     if r.status_code == 200:
-        print(r.json()["message"])
+        return r.json()["session"]
     else:
-        print(f"{r.status_code}: {r.json()["detail"]}")
+        raise ConnectionError(f"{r.status_code}:{r.json()["detail"]}")
+    
+def getTasks(token:uuid.UUID):
+    r = requests.get(f"{url}/all-tasks?token={token}")
 
-login()
+    if r.status_code == 200:
+        return r.text
+    else:
+        raise ConnectionError(f"{r.status_code}:{r.json()["detail"]}")
+
+
+
+
+
+
+while True:
+    try:
+        session = login()
+        token = uuid.UUID(session["token"])
+        break
+    except ConnectionError as e:
+        print(e)
+
+try:
+    tasks = getTasks(token)
+    print(tasks)
+except ConnectionError as e:
+    print(e)
